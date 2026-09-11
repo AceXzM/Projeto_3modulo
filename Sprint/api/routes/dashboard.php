7d02ca76542291ddd8c799d87e4a1b261be6ddd3
Lucas <145052396+AceXzM@users.noreply.github.com>
2026-09-10 22:23:30 -0300

    Delete Sprint directory

--- Sprint/api/routes/dashboard.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-
-require_once __DIR__ . '/../controllers/DashboardController.php';
-
-// GET /api/dashboard/indicadores?busca=&categoria_id=&limite=&offset=
-DashboardController::indicadores();
